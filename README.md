b7e933fea3e3f9ea8f615ba62f47299a71e7646f
Matías Cánepa <matias.canepa@gmail.com>
2019-05-23 14:36:30 -0300

    Actual working code with instructions

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 This is just a joke.
+
+##Steps to install
+
+1. Place `bg.sh` inside user's home directory.
+2. Replace "target_victim" with the actual user name.
+3. Run `chmod u+x bg.sh`
+4. Set a cron like: `* * * * * /home/user/bg.sh` where user is the actual user name.
--- bg.sh
+++ bg.sh
@@ -1,3 +1,20 @@
-cd /tmp
-wget http://url.to/image.png  -Obg.png
-gsettings set org.gnome.desktop.background picture-uri file:///tmp/bg.png
+#!/bin/bash
+
+#Set variables
+USER=target_victim
+FILE=bg.jpg
+FILE_PATH=/home/$USER/$FILE
+URL=http://url.to/image.png
+
+#Download picture to user's home directory
+wget $URL -O $FILE
+
+#We need some variables to be exported so cron can change the wallpaper
+PID=$(pgrep -u $USER gnome-session)
+
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+
+sleep 5
+
+#Now change the wallpaper image
+gsettings set org.gnome.desktop.background picture-uri file://$FILE_PATH
